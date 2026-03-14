b57fb26ab05d95aa223c70d836f4f6e984a90c87
Aleksandr Batyrev <aiiiotus99i@gmail.com>
2026-03-14 12:49:28 +0500

    [Sprint-1][Task-1-6]feat(auth): implement user authentication routes and handlers

---
+++ auth-service/tests/api/routes/signup.rs
@@ -0,0 +1,46 @@
+use crate::helpers::{get_random_email, TestApp};
+
+#[tokio::test]
+async fn should_return_422_if_malformed_input() {
+    let app = TestApp::new().await;
+
+    let random_email = get_random_email();
+
+    let test_cases = [
+        serde_json::json!({
+            "password": "password123",
+            "requires2FA": true
+        }),
+        serde_json::json!({
+            "password": "",
+            "requires2FA": false
+        }),
+        serde_json::json!({
+            "password": "397469834@%#%@#%@",
+            "requires2FA": true
+        }),
+        serde_json::json!({
+            "password": "parkour123",
+            "requires2FA": false
+        }),
+        serde_json::json!({
+            "password": "exec(rm -rf /)",
+            "requires2FA": false
+        }),
+        serde_json::json!({
+            "email": random_email,
+            "password": "password123",
+            "requires2FA": true
+        }),
+    ];
+
+    for test_case in test_cases.iter() {
+        let response = app.post_signup(test_case).await;
+        assert_eq!(
+            response.status().as_u16(),
+            422,
+            "Failed for input: {:?}",
+            test_case
+        );
+    }
+}
